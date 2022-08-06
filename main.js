d2f306c82f36006f7e4995fa67f117deb7832b44
Michael Xie <mxie0114@gmail.com>
2022-08-06 13:43:57 -0500

    added deploy-command.js ; will need to work on it

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-
-const { token } = require('./config.json');
-
+// All necessary information for bot
+const { Client, GatewayIntentBits, Message, messageLink, SlashCommandBuilder } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const discord = require('discord.js');
+const { token } = require('./sensitive/config.json');
 const client = new Client({ intents: [GatewayIntentBits.Guilds]});
 
 client.once('ready', () => {
-    console.log("I'm Alive!");
+    console.log(client.user.tag + "Now Online");
 });
 
 client.login(token);
