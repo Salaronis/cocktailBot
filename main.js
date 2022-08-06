df4c4f91b2875266ac180db2dfffd74552887538
Michael Xie <mxie0114@gmail.com>
2022-08-06 12:36:21 -0500

    First Commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds]});
+
+client.once('ready', () => {
+    console.log("I'm Alive!");
+});
+
+client.login(token);
