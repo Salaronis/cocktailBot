f3c5fbc384041d0b2aa22efaf0ce9a2c231729c2
Michael Xie <mxie0114@gmail.com>
2022-08-08 21:44:17 -0500

    working messageCreate, adding backbone. and starting slash commands

--- main.js
+++ main.js
@@ -7,20 +7,36 @@ const { Intents } = require('discord.js')
 const client = new discord.Client({ intents: [GatewayIntentBits.GuildMessages, GatewayIntentBits.Guilds,  GatewayIntentBits.MessageContent]})
 
 const prefix = 'cb';
-
+const maxIn = 5;
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
   });
   
   client.on('messageCreate', (msg) => {
-    if(!msg.content.startsWith(prefix) || msg.author.bot == true){
-        return;
-    }  
-    let actual = msg.content.substring(prefix.length + 1);
-    console.log(actual);
-    if(actual === 'margherita'){
-        msg.reply('Here is a margherita recipe!');
+    const input = msg.content;
+    const cmds = input.split();
+    console.log(cmds[0]);
+    console.log(!(cmds[0] === 'prefix'));
+    if(!(cmds[0] === 'prefix') || msg.author.bot) return;
+
+    if(cmds[1] === 'random'){
+
     }
+    if(cmds.length > maxIn){
+        msg.reply('Too Many Words!, Please input '+ value +' words including the prefix: cB');
+    }
+    /*
+    if(cmds[0] === 'margarita'){
+        msg.reply('Here is a margarita recipe!');
+    }
+    */
+})
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	if (interaction.commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
 })
 
 client.login(token);
