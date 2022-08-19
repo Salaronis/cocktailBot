9fb6d667e5f8352290e9defdb351d9c1b8f1e6a6
Michael Xie <mxie0114@gmail.com>
2022-08-19 15:22:45 -0500

    Added Axios Functionality; working requests from API

--- main.js
+++ main.js
@@ -5,16 +5,29 @@ const discord = require('discord.js')
 const { token } = require('./sensitive/config.json')
 const { Intents } = require('discord.js')
 const client = new discord.Client({ intents: [GatewayIntentBits.GuildMessages, GatewayIntentBits.Guilds,  GatewayIntentBits.MessageContent]})
-
+const axios = require("axios");
 const prefix = 'cb';
 const maxIn = 5;
 
+
 const options = {
-    method: 'GET',
-    headers: {
-        'Key': "1'"
-    }
+  method: 'GET',
+  url: 'https://the-cocktail-db.p.rapidapi.com/filter.php',
+  params: {i: 'Gin'},
+  headers: {
+    'X-RapidAPI-Key': '7e9d60ad24msh3331a3ff8a0940ap112b3djsn599d0014be83',
+    'X-RapidAPI-Host': 'the-cocktail-db.p.rapidapi.com'
+  }
 };
+
+/*
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+
+*/
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -26,32 +39,36 @@ client.on('ready', () => {
     console.log(cmds);
     if(!(cmds[0] === prefix) || msg.author.bot) return;
 
+    const info = "";
     if(cmds[1] === 'random'){
         msg.reply('Here\'s a recipe I think you\'ll like');
 
-        fetch('www.thecocktaildb.com/api/json/v1/1/random.php', options)
-            .then(response => response.json())
-            .then(response => console.log(response))
-            .catch(err => console.error(err));
+        options.url = "https://the-cocktail-db.p.rapidapi.com/random.php";
+        axios.request(options).then(function (response) {
+            console.log(response.data);
+            info = JSON.parse(response.data);
+            msg.reply(info.strDrink);
+        }).catch(function (error) {
+            console.error(error);
+        });
     }
-    if(cmds[1] === 'margarita'){
-        msg.reply('Here is a margarita recipe');
-    }
+    if(cmds[1] === 'ing')
+        options.url = 'https://the-cocktail-db.p.rapidapi.com/filter.php';
+        options.params.i = cmds[2];
     if(cmds.length > maxIn){
         msg.reply('Too Many Words!, Please input '+ value +' words including the prefix: cb');
     }
-    /*
-    if(cmds[0] === 'margarita'){
-        msg.reply('Here is a margarita recipe!');
-    }
-    */
+
 })
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
-	if (interaction.commandName === 'ping') {
-		await interaction.reply('Pong!');
+	if (interaction.commandName === 'help') {
+		await interaction.reply('This Application allows you to request a cocktail Recipe, provided from TheCocktailDB');
 	}
+    if(interaction.commandName === ''){
+
+    }
 })
 
 client.login(token);
